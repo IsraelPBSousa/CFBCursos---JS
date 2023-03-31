018c88ca20a128f2da14a066b02d18e55672fe38
Israel Borges <israelpborges@gmail.com>
2023-03-31 15:34:51 -0300

    Create aula22.js

---
+++ aula22.js
@@ -0,0 +1,23 @@
+// Const VALOR_PADRAO=0
+
+function add(v){
+    valor+=v
+}
+
+let valor=0
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
+
+// function soma(n1=0,n2=0){
+//     let res
+//     res=n1+n2
+//     return res
+// }
+
+// let resultado_soma=soma(5,5)
+//  console.log(resultado_soma)
