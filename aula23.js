23736b74b01446ea61f8180ccc3a82e63e67683a
Israel Borges <israelpborges@gmail.com>
2023-03-31 22:19:32 -0300

    Update aula23.js

--- aula23.js
+++ aula23.js
@@ -1,4 +1,8 @@
 function soma(...valores){
-    return valores.length
+    let res=0
+    for(let v of valores){
+    res+=v
+    }
+    return res
 }
-console.log(soma(10,5,2,15,8))
+console.log(soma(10,5))
